062bf1753efc5860675500966506cd5022ec0060
nahidn228 <nahidn228@gmail.com>
2025-06-29 22:28:39 +0600

    add schadcn UI and router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl">Vite + React</h1>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import App from "../App";
+
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default router;
